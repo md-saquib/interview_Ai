f841de8bd39d60e5bf4accdd29b5504c5dc54c03
saquib <ahmed.saquib.8910@gmail.com>
2026-04-30 03:03:50 +0530

    implement deployed backend link by Render...

--- Frontend/src/services/aiServices.js
+++ Frontend/src/services/aiServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/interview";
+const API_URL = "https://interview-ai-vwq3.onrender.com/api/interview";
 
 
 export const generateInterviewReport = async (jobDescription, resume, selfDescription, selectedModel) => {
@@ -76,4 +76,4 @@ export const downloadResumePdf = async (interviewReportId) => {
     } catch (error) {
         throw new Error(error.response?.data?.message || 'Failed to download resume PDF');
     }
-};
+};
--- Frontend/src/services/authServices.js
+++ Frontend/src/services/authServices.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const API_URL = 'http://localhost:3000/api/auth';
+const API_URL = 'https://interview-ai-vwq3.onrender.com/api/auth';
 
 export const register = async (credentials) => {
     try {
