e3099f909e2d3c36e5c8cc79182452c9f9352f1d
saquib <ahmed.saquib.8910@gmail.com>
2026-05-07 13:54:26 +0530

    remove token bug and update node enviroment in render.com

--- Frontend/src/services/aiServices.js
+++ Frontend/src/services/aiServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/interview";
+const API_URL = `${import.meta.env.VITE_API_URL}/api/interview`;
 
 
 export const generateInterviewReport = async (jobDescription, resume, selfDescription, selectedModel) => {
--- Frontend/src/services/authServices.js
+++ Frontend/src/services/authServices.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 
-const API_URL = 'http://localhost:3000/api/auth';
+const API_URL = `${import.meta.env.VITE_API_URL}/api/auth`;
 
 export const register = async (credentials) => {
     try {
